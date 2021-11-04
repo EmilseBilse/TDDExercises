94ea03d2c4d2bab541be304a07914a1f9fb1b4df
EmilseBilse <emil875b@easv365.dk>
2021-11-04 10:42:12 +0100

    :)

--- WeatherExercisesTest/WeatherTests.cs
+++ WeatherExercisesTest/WeatherTests.cs
@@ -42,21 +42,21 @@ namespace WeatherExercisesTest
             //Assert
             Assert.Equal(expected, cb.ContainsHeavyRain(rain));
         }
-        
+
         [Fact]
         public void TrueHeavyRain()
         {
             //Arrange
             ICloudburst cb = new Cloudburst();
-            double[] rain = {1,0,0,2,1,1,0,0,0,0,1,1,0,1,2,0,2,1};
+            double[] rain = {1, 0, 0, 2, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 2, 0, 2, 1};
             //Act
             int expected = 3;
-            
+
             //Assert
             Assert.Equal(expected, cb.ContainsHeavyRain(rain));
-        
-        
-        
-        
+        }
+
+
+
     }
 }
